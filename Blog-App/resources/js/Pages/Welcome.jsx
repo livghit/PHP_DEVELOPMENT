175a5261b48abd91ee98275b64b4269621516daa
livghit <liviu.g01@gmail.com>
2022-12-10 15:04:12 +0100

    Webblog added to this repo !

---
+++ Blog-App/resources/js/Pages/Welcome.jsx
@@ -0,0 +1,35 @@
+import { Link, Head } from '@inertiajs/inertia-react';
+
+export default function Welcome(props) {
+    return (
+        <>
+            <Head title="Welcome" />
+            <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+                <div className="fixed top-0 right-0 px-6 py-4 sm:block">
+                    {props.auth.user ? (
+                        <Link href={route('dashboard')} className="text-sm text-gray-700 dark:text-gray-200 underline">
+                            Dashboard
+                        </Link>
+                    ) : (
+                        <>
+                            <Link href={route('login')} className="text-sm text-gray-700 dark:text-gray-200 underline">
+                                Log in
+                            </Link>
+
+                            <Link
+                                href={route('register')}
+                                className="ml-4 text-sm text-gray-700 dark:text-gray-200 underline"
+                            >
+                                Register
+                            </Link>
+                        </>
+                    )}
+                </div>
+
+                <div className='text-white text-4xl'>Welcome Miniblog Page !</div>
+
+
+            </div>
+        </>
+    );
+}
