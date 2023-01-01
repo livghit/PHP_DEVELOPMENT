f4a7ac20e5a0ece056733e96ca4b93e78764a18b
livghit <liviu.g01@gmail.com>
2023-01-01 20:44:12 +0100

    Tested the debugbar

--- E-Comerce-Project/routes/web.php
+++ E-Comerce-Project/routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +13,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
