bd13f86d08426f7763ad6949ab058ab4c46abebc
livghit <liviu.g01@gmail.com>
2023-01-01 20:39:34 +0100

    Configurated the Debugbar

--- E-Comerce-Project/app/Http/Kernel.php
+++ E-Comerce-Project/app/Http/Kernel.php
@@ -36,6 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Barryvdh\Debugbar\Middleware\InjectDebugbar::class,
         ],
 
         'api' => [
